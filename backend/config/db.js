531577aa23efd1fd40b307e126fd0b7e4f0fb2e3
shabbeerali786 <shabbeeralij.23it@kongu.edu>
2025-06-24 10:17:32 +0530

    day7

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 
-export const connectDB = async () => {
+export const 
+connectDB = async () => {
     try {
         await mongoose.connect('mongodb://localhost:27017/blogging');
         console.log('MongoDB connected');
